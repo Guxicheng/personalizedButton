4b010e2464c2bd7bf415936e672be8af22911215
zhaozhenxing <zhenxing.zhao@ucas.com.cn>
2022-03-13 02:01:28 +0800

    first commit 完成基础个性化按钮的封装

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>
+      个性化按钮
+    </h1>
+    <lu-button>默认按钮</lu-button>
+    <lu-button type="primary">主要按钮</lu-button>
+    <lu-button type="success">成功按钮</lu-button>
+    <lu-button type="info">信息按钮</lu-button>
+    <lu-button type="warning">警告按钮</lu-button>
+    <lu-button type="danger">危险按钮</lu-button>
+  </div>
+</template>
+
+<script>
+import luButton from '../src/buttons/lu-button'; // 引入
+
+export default {
+  name: 'App',
+  components: {
+    luButton
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/buttons/lu-button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="lu-button"
+  :class="[type ? 'lu-button--' + type : '']"
+  @click="handleClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props:{
+    type:{
+      type:String,
+      default:""
+    }
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt);
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import "./button.css";
+</style>
